f53702ffc259f87db8f055bcac38dc5c08315ac8
Zhang, En-Yao <zhang_enyao@icloud.com>
2026-03-05 23:36:38 +0800

    refactor: remove values-widget component and integrate editor with markdown support

--- src/lib/components/dynamic-form/widgets/editor.svelte
+++ src/lib/components/dynamic-form/widgets/editor.svelte
@@ -1,141 +1,73 @@
 <script lang="ts" module>
+	import shell from 'highlight.js/lib/languages/shell';
+	import yaml from 'highlight.js/lib/languages/yaml';
+	import 'highlight.js/styles/github.css';
+	import rehypeHighlight from 'rehype-highlight';
+	import type { Plugin } from 'svelte-exmarkdown';
+	import Markdown from 'svelte-exmarkdown';
+	import { gfmPlugin } from 'svelte-exmarkdown/gfm';
+	import Monaco from 'svelte-monaco';
+
+	import * as Resizable from '$lib/components/ui/resizable';
 	import '@sjsf/form/fields/extra-widgets/combobox';
 
-	import type { Command } from 'bits-ui';
-
 	import type { ButtonProps } from '$lib/components/ui/button/button.svelte';
-	export type ComboboxEnumeration = {
-		label: string;
-		value: string;
-		disabled?: boolean;
-	};
+
 	declare module '@sjsf/form' {
 		interface UiOptions {
-			TailoredComboboxEnumerations?: (filterValue: string) => Promise<ComboboxEnumeration[]>;
-			TailoredComboboxVisibility?: number;
-			TailoredComboboxTrigger?: ButtonProps;
-			TailoredComboboxInput?: Command.InputProps;
-			TailoredComboboxEmptyText?: string;
+			TailoredEditorDocument?: string;
+			TailoredEditorTrigger?: ButtonProps;
 		}
 	}
 </script>
 
 <script lang="ts">
-	import { PlusIcon, SearchXIcon } from '@lucide/svelte';
-	import Check from '@lucide/svelte/icons/check';
-	import ChevronsUpDown from '@lucide/svelte/icons/chevrons-up-down';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import {
 		ariaInvalidProp,
 		type ComponentProps,
 		composeProps,
 		disabledProp,
 		getFormContext,
-		getPseudoId,
 		handlersAttachment,
-		inputAttributes,
 		retrieveUiOption,
 		uiOptionProps
 	} from '@sjsf/form';
-	import { idMapper, singleOption } from '@sjsf/form/options.svelte';
-	import { getThemeContext } from '@sjsf/shadcn4-theme';
-	import { tick } from 'svelte';
 
-	import CommandSeparator from '$lib/components/ui/command/command-separator.svelte';
-	import * as Empty from '$lib/components/ui/empty/index.js';
-	import { cn } from '$lib/utils.js';
 	const ctx = getFormContext();
-	const themeCtx = getThemeContext();
-	const {
-		Popover,
-		PopoverContent,
-		PopoverTrigger,
-		Button,
-		Command: CommandRoot,
-		CommandInput,
-		CommandList,
-		CommandGroup,
-		CommandItem
-	} = $derived(themeCtx.components);
-	let {
-		value = $bindable(),
-		config,
-		handlers,
-		options
-	}: ComponentProps['comboboxWidget'] = $props();
+
+	let { value = $bindable(), config, handlers }: ComponentProps['textWidget'] = $props();
+
 	const { oninput, onchange, ...buttonHandlers } = $derived(handlers);
 
-	let filterValue = $state((value as string) ?? '');
-	let enumerations = $state<ComboboxEnumeration[]>([]);
-
-	const enumerationsProvider = $derived(
-		retrieveUiOption(ctx, config, 'TailoredComboboxEnumerations') as (
-			filterValue: string
-		) => Promise<ComboboxEnumeration[]>
-	);
-	$effect(() => {
-		if (enumerationsProvider) {
-			enumerationsProvider(filterValue)
-				.then((result) => {
-					enumerations = result ?? [];
-				})
-				.catch((error) => {
-					console.error('Failed to fetch enumerations:', error);
-					enumerations = [];
-				});
+	const plugins: Plugin[] = [
+		gfmPlugin(),
+		{
+			rehypePlugin: [
+				rehypeHighlight,
+				{
+					ignoreMissing: true,
+					languages: { shell, yaml }
+				}
+			]
 		}
-	});
+	];
 
-	const optionsProxy = $derived(
-		enumerations
-			? enumerations.map((enumeration, index) => ({
-					id: getPseudoId(ctx, config.path, index),
-					label: enumeration.label,
-					value: enumeration.value,
-					disabled: enumeration.disabled ?? false
-				}))
-			: options
+	const document: string = $derived(
+		retrieveUiOption(ctx, config, 'TailoredEditorDocument') as string
 	);
-	const optionsManager = singleOption({
-		mapper: () => idMapper(optionsProxy),
-		value: () => value,
-		update: (v) => (value = v)
-	});
 
-	const visibility: number = $derived(
-		retrieveUiOption(ctx, config, 'TailoredComboboxVisibility') as number
-	);
-	let visibleOptions = $derived(visibility);
-	const filteredOptions = $derived(
-		optionsProxy.filter((option) => option.label.includes(filterValue))
-	);
+	let valueProxy = $state(value ?? '');
 	$effect(() => {
-		if (open === false) {
-			filterValue = '';
-			visibleOptions = visibility;
-		}
+		value = valueProxy;
 	});
-
-	const attributes = $derived(inputAttributes(ctx, config, 'TailoredComboboxInput', handlers, {}));
-	const triggerContent = $derived(value ?? attributes.placeholder);
-
-	const emptyText = $derived(retrieveUiOption(ctx, config, 'TailoredComboboxEmptyText'));
 
 	let open = $state(false);
-	let triggerReference = $state<HTMLButtonElement>(null!);
-	function closeAndFocusTrigger() {
-		open = false;
-		tick().then(() => {
-			triggerReference.focus();
-		});
-	}
 </script>
 
-<Popover bind:open>
-	<PopoverTrigger
-		class="w-full justify-between"
-		bind:ref={triggerReference}
-		{...disabledProp({}, config, ctx)}
-	>
+<Dialog.Root bind:open>
+	<Dialog.Trigger class="w-full justify-between" {...disabledProp({}, config, ctx)}>
 		{#snippet child({ props })}
 			<Button
 				{...composeProps(
@@ -147,66 +79,113 @@
 						role: 'combobox',
 						'aria-expanded': open
 					} satisfies ButtonProps,
-					uiOptionProps('TailoredComboboxTrigger'),
+					uiOptionProps('TailoredEditorTrigger'),
 					handlersAttachment(buttonHandlers),
 					ariaInvalidProp
 				)}
 			>
-				<span>
-					{triggerContent}
-				</span>
-				<ChevronsUpDown class="ml-2 size-4 shrink-0 opacity-50" />
+				<p class="w-full text-center">Edit</p>
 			</Button>
 		{/snippet}
-	</PopoverTrigger>
-	<PopoverContent class="w-[200px] p-0">
-		<CommandRoot shouldFilter={false}>
-			<CommandInput bind:value={filterValue} />
-			<CommandList>
-				{#if filteredOptions.length === 0}
-					<Empty.Root>
-						<Empty.Header>
-							<Empty.Media>
-								<SearchXIcon />
-							</Empty.Media>
-							<Empty.Description>{emptyText}</Empty.Description>
-						</Empty.Header>
-					</Empty.Root>
-				{/if}
-				<CommandGroup>
-					{#each filteredOptions.slice(0, visibleOptions) as option (option.id)}
-						<CommandItem
-							value={option.value as string}
-							onSelect={() => {
-								optionsManager.current = option.id;
-								oninput?.();
-								onchange?.();
-								closeAndFocusTrigger();
-							}}
-							disabled={option.disabled}
-						>
-							<Check
-								class={cn(
-									'mr-2 size-4',
-									optionsManager.current !== option.id && 'text-transparent'
-								)}
-							/>
-							{option.label}
-						</CommandItem>
-					{/each}
-				</CommandGroup>
-				{#if filteredOptions.length > visibleOptions}
-					<CommandSeparator />
-					<CommandItem
-						class="flex w-full items-center justify-center rounded-t-none hover:bg-muted"
-						onclick={() => {
-							visibleOptions += visibility;
-						}}
-					>
-						<PlusIcon />
-					</CommandItem>
-				{/if}
-			</CommandList>
-		</CommandRoot>
-	</PopoverContent>
-</Popover>
+	</Dialog.Trigger>
+	<Dialog.Content class="max-h-[95vh] min-h-[95vh] max-w-[95vw] min-w-[95vw]">
+		<Resizable.PaneGroup direction="horizontal">
+			<Resizable.Pane defaultSize={50}>
+				<div class="markdown h-full overflow-auto">
+					<Markdown {plugins} md={document} />
+				</div>
+			</Resizable.Pane>
+			<Resizable.Handle withHandle />
+			<Resizable.Pane defaultSize={50}>
+				<Monaco
+					options={{
+						language: 'yaml',
+						padding: { top: 32, bottom: 8 },
+						automaticLayout: true
+					}}
+					theme="vs-dark"
+					bind:value={valueProxy}
+				/>
+			</Resizable.Pane>
+		</Resizable.PaneGroup>
+	</Dialog.Content>
+</Dialog.Root>
+
+<style lang="postcss">
+	@reference "tailwindcss";
+
+	.markdown :global(h1) {
+		@apply mt-8 mb-4 border-b border-gray-200 pb-2 text-2xl font-bold;
+	}
+	.markdown :global(h2) {
+		@apply mt-8 mb-4 border-b border-gray-200 pb-2 text-xl font-bold;
+	}
+	.markdown :global(h3) {
+		@apply mt-5 mb-2 text-lg font-semibold;
+	}
+	.markdown :global(h4) {
+		@apply mt-4 mb-2 text-base font-semibold;
+	}
+	.markdown :global(h5) {
+		@apply mt-4 mb-2 text-sm font-semibold;
+	}
+	.markdown :global(h6) {
+		@apply mt-4 mb-2 text-xs font-semibold text-gray-600;
+	}
+	.markdown :global(p) {
+		@apply mb-4;
+	}
+	.markdown :global(a) {
+		@apply break-words text-blue-600 underline transition-colors hover:text-blue-800;
+	}
+	.markdown :global(ul),
+	.markdown :global(ol) {
+		@apply mb-4 pl-8;
+	}
+	.markdown :global(ul) > :global(li) {
+		@apply list-disc;
+	}
+	.markdown :global(ol) > :global(li) {
+		@apply list-decimal;
+	}
+	.markdown :global(li) {
+		@apply mb-1;
+	}
+	.markdown :global(blockquote) {
+		@apply mb-4 rounded border-l-4 border-gray-300 bg-gray-50 pl-4 text-gray-700;
+	}
+	.markdown :global(pre) {
+		@apply mb-4 overflow-x-auto rounded bg-gray-100 p-4 text-sm leading-relaxed;
+	}
+	.markdown :global(code) {
+		@apply rounded bg-gray-200 px-1 py-0.5 font-mono text-sm;
+	}
+	.markdown :global(pre) :global(code) {
+		@apply m-0 rounded-none bg-transparent p-0 text-inherit;
+	}
+	.markdown :global(table) {
+		@apply my-6 w-full border-collapse;
+	}
+	.markdown :global(th),
+	.markdown :global(td) {
+		@apply border border-gray-200 px-4 py-2;
+	}
+	.markdown :global(th) {
+		@apply bg-gray-50 font-semibold;
+	}
+	.markdown :global(img) {
+		@apply h-auto max-w-full rounded;
+	}
+	.markdown :global(hr) {
+		@apply my-8 border-t border-gray-200;
+	}
+	.markdown :global(strong) {
+		@apply font-bold;
+	}
+	.markdown :global(em) {
+		@apply italic;
+	}
+	.markdown :global(del) {
+		@apply line-through;
+	}
+</style>
