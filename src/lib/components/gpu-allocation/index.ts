4db1f16d7aa085477e9cf5704fb7d4d57ea5309c
woody_lin <woody_lin@phison.com>
2026-03-31 21:31:42 +0800

    feat: add model sservice select gpu

--- src/lib/components/gpu-allocation/index.ts
+++ src/lib/components/gpu-allocation/index.ts
@@ -1,3 +1,4 @@
+export { fetchAllGpuNodes } from './fetch-topology';
 export { default as GpuAllocationDiagram } from './gpu-allocation-diagram.svelte';
 export { default as GpuAllocationDialog } from './gpu-allocation-dialog.svelte';
 export type * from './types';
