7570b306579b821751b090148bddeac181ea3669
woody_lin <woody_lin@phison.com>
2026-04-13 21:22:13 +0800

    fix: GPU Placement validation fails when node data is unavailable due to empty enum arrays

--- src/lib/components/kind-viewer/kind-viewer-actions/model-service/create.svelte
+++ src/lib/components/kind-viewer/kind-viewer-actions/model-service/create.svelte
@@ -639,7 +639,9 @@
 								title: 'Node',
 								type: 'string',
 								description: 'Select a GPU node (nodeSelector: kubernetes.io/hostname)',
-								enum: nodeNameOptions.map((o) => o.value)
+								...(nodeNameOptions.length > 0 && {
+									enum: nodeNameOptions.map((o) => o.value)
+								})
 							},
 							gpuType: {
 								title: 'GPU Type',
@@ -647,7 +649,9 @@
 								description: 'Select GPU types (annotation: nvidia.com/use-gputype)',
 								items: {
 									type: 'string',
-									enum: gpuTypeOptions().map((o) => o.value)
+									...(gpuTypeOptions().length > 0 && {
+										enum: gpuTypeOptions().map((o) => o.value)
+									})
 								},
 								uniqueItems: true
 							},
@@ -657,7 +661,9 @@
 								description: 'Select GPU UUIDs (annotation: nvidia.com/use-gpuuuid)',
 								items: {
 									type: 'string',
-									enum: gpuUuidOptions.map((o) => o.value)
+									...(gpuUuidOptions.length > 0 && {
+										enum: gpuUuidOptions.map((o) => o.value)
+									})
 								},
 								uniqueItems: true
 							}
