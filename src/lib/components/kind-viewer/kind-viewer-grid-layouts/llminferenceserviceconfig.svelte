9860b3fe14b641ca094923a40f75c91b1a3f070f
george-wu20250203 <george_wu@phison.com>
2026-05-24 23:37:28 +0800

    refactor: refine UI

--- src/lib/components/kind-viewer/kind-viewer-grid-layouts/llminferenceserviceconfig.svelte
+++ src/lib/components/kind-viewer/kind-viewer-grid-layouts/llminferenceserviceconfig.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import type { JsonValue } from '@bufbuild/protobuf';
-	import BrainCircuitIcon from '@lucide/svelte/icons/brain-circuit';
+	import { BotIcon } from '@lucide/svelte';
 	import type { ServingKserveIoV1Alpha2LLMInferenceServiceConfig } from '@otterscale/types';
 	import type { Schema } from '@sjsf/form';
 	import type { Row } from '@tanstack/table-core';
 	import type { ValidateFunction } from 'ajv';
 
-	import * as Avatar from '$lib/components/ui/avatar/index.js';
+	import { Badge } from '$lib/components/ui/badge/index.js';
 	import * as Card from '$lib/components/ui/card/index.js';
 	import * as Item from '$lib/components/ui/item';
 
@@ -41,21 +41,17 @@
 	const rowNamespace = $derived(object?.metadata?.namespace ?? namespace);
 </script>
 
-<Card.Root>
+<Card.Root class="transition-shadow hover:shadow-md">
 	<Card.Header>
 		<Item.Root class="items-start p-0">
-			<Item.Media>
-				<Avatar.Root>
-					<Avatar.Fallback>
-						<BrainCircuitIcon class="size-4" />
-					</Avatar.Fallback>
-				</Avatar.Root>
+			<Item.Media class="rounded-full bg-muted p-2">
+				<BotIcon />
 			</Item.Media>
-			<Item.Content class="text-left">
-				<Item.Title class="font-bold">
+			<Item.Content class="min-w-0 text-left">
+				<Item.Title class="text-base font-semibold">
 					{row.original['Name']}
 				</Item.Title>
-				<Item.Description>
+				<Item.Description class="flex items-center gap-1 text-xs text-muted-foreground">
 					{row.original['Namespace']}
 				</Item.Description>
 			</Item.Content>
@@ -74,20 +70,34 @@
 			</Item.Actions>
 		</Item.Root>
 	</Card.Header>
-	<Card.Content class="flex flex-1 flex-col gap-2">
+	<Card.Content class="flex flex-1 gap-3">
 		{@const modelName = row.original['Model Name'] as string | null}
-		{#if modelName}
-			<p>{modelName}</p>
-		{/if}
 		{@const modelUri = row.original['Model URI'] as string | null}
-		{#if modelUri}
-			<p>{modelUri}</p>
+		{#if modelName}
+			<Item.Root class="items-start p-0">
+				<Item.Content class="min-w-0 text-left">
+					<Item.Title>Model</Item.Title>
+					<Item.Description>
+						{modelName}
+					</Item.Description>
+				</Item.Content>
+			</Item.Root>
+			<Item.Root class="items-start p-0">
+				<Item.Content class="min-w-0 text-left">
+					<Item.Title>Source</Item.Title>
+					<Item.Description>
+						{modelUri}
+					</Item.Description>
+				</Item.Content>
+			</Item.Root>
 		{/if}
 	</Card.Content>
 	<Card.Footer class="flex flex-wrap items-center gap-2 text-xs">
 		{@const mode = row.original['Mode'] as string}
 		{#if mode}
-			<p>{mode}</p>
+			<Badge>
+				{mode}
+			</Badge>
 		{/if}
 	</Card.Footer>
 </Card.Root>
