3b5a90e94f6ba8bafd89fe34c0906075ed8d82c6
Cody Chen <cdxvy30@gmail.com>
2026-08-10 19:01:54 +0800

    fix(dev-only): remove workspace trigger

--- src/lib/components/layout/create-workspace-trigger.svelte
+++ src/lib/components/layout/create-workspace-trigger.svelte
@@ -1,17 +1,9 @@
 <script lang="ts">
-	import PlusIcon from '@lucide/svelte/icons/plus';
-
-	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
-	import { m } from '$lib/messages';
-
 	let {
 		open = $bindable()
 	}: {
 		open: boolean;
 	} = $props();
-</script>
 
-<DropdownMenu.Item class="gap-2 p-2" onSelect={() => (open = true)}>
-	<PlusIcon class="size-4" />
-	{m.add_workspace()}
-</DropdownMenu.Item>
+	void open;
+</script>
