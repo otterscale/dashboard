8ef2439183c9bd688e834e88b9b4dd8fc5e38b38
Zhang, En-Yao <76697563+ZhangEnYao@users.noreply.github.com>
2026-08-20 16:52:53 +0800

    Feat refine searchbar (#636)
    * feat(dynamic-table): replace Filtrex with Liqe for query parsing and update documentation
    
    * feat(dynamic-table): add dynamic table search documentation component
    
    * feat: refactor search
    
    * feat(dynamic-table): refactor global filter handling in dynamic table
    
    ---------
    
    Co-authored-by: Zhang <zhangen-yao@ZhangdeMacBook-Pro.local>
    (cherry picked from commit 486dba05e3b1d2545ea7849adf2833ef024dec20)

--- src/lib/components/dynamic-table/index.ts
+++ src/lib/components/dynamic-table/index.ts
@@ -1,5 +1,6 @@
 import DynamicTable from './dynamic-table.svelte';
 import DynamicTableCell from './dynamic-table-cell.svelte';
 import DynamicTableHeader from './dynamic-table-header.svelte';
+import DynamicTableSearchDocument from './dynamic-table-search-document.svelte';
 
-export { DynamicTable, DynamicTableCell, DynamicTableHeader };
+export { DynamicTable, DynamicTableCell, DynamicTableHeader, DynamicTableSearchDocument };
